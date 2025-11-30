611281427f93d779d332dff9ec94e9e74ef1a9e8
paula-cediel <mariapaulacediel08@gmail.com>
2025-11-30 16:54:46 -0500

    Add files via upload

---
+++ ejercicio1.js
@@ -0,0 +1,12 @@
+//string
+let nombre = "maria paula";
+//number
+let edad = 18;
+//Boolean
+let esEstudiante = true;
+//undefined
+let sinValor;
+//null
+let valorNulo = null;
+
+console.log(nombre, edad, esEstudiante, sinValor, valorNulo); 
---
+++ ejercicio2.js
@@ -0,0 +1,12 @@
+let añoNacimiento = prompt("¿En qué año naciste?");
+let edadActual = 2025 - Number(añoNacimiento);
+
+console.log("Tu edad es: " + edadActual);
+
+let num1 = prompt("Ingresa un número:");
+let num2 = prompt("Ingresa otro número:");
+
+let suma = Number(num1) + Number(num2);
+
+console.log("La suma es: " + suma);
+console.log("añoNacimiento, edadActual, num1, num2, suma");
